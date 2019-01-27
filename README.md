5c8997e79dadcf535160780fd8a69d10caac7ed6
Rob Charlton <rob@infonimbus.com>
2019-01-27 18:58:17 +0000

    updated the collision detection to be more generally useful

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dad_jumper
+
+Beginnings of a simple platform game in pygame zero. Use left and right arrow with up to jump.
+
+To start the game just type pgzrun main.py.
--- main.py
+++ main.py
@@ -12,18 +12,59 @@ STANDPOS = 5
 BUMPPOS = 8
 
 def make_ground(x, y, width):
+    ''' 
+        return a list of Actors of type ground starting at (x,y) and being
+        at least width wide 
+    '''
     ground = []
     for i in range(int(width / images.ground.get_width())):
         ground.append(Actor('ground',
             topleft=(x + images.ground.get_width() * i, y)))
     return ground
 
+def collide(rect1, dx, dy, rect2):
+    '''
+        Check if rect1 will collide with rect2, specifically to check that
+        when it moves by dx left or right and dy up or down that
+        - rect1 is on top of rect2 (standing)
+        - rect1 is to the left or right of rect2 (blocked from moving that way)
+        - rect1 is underneath rect2 (underneath and bumping up into it)
+
+        return:
+        - (Standing, ypos)
+        - (LeftOf, xpos)
+        - (RightOf, xpos)
+        - (Under, ypos)
+    '''
+    testrect = rect1.copy().move(dx, dy)
+    if testrect.colliderect(rect2):
+        botoverlap = testrect.bottom - rect2.top
+        topoverlap = rect2.bottom - testrect.top
+        rightoverlap = testrect.right - rect2.left
+        leftoverlap = rect2.right - testrect.left
+        #print("Bot=%f Top=%f Left=%f Right=%f dx=%f dy=%f" % (botoverlap, topoverlap, leftoverlap, rightoverlap, dx, dy))
+        if botoverlap > 0 and botoverlap <= abs(dy):
+            # standing on rect2
+            return ('standing', rect2.top)
+        elif topoverlap > 0 and topoverlap <= abs(dy):
+            # bumping up into rect2
+            return ('under', rect2.bottom)
+        elif rightoverlap > 0 and rightoverlap <= abs(dx):
+            # bumped into rect2 on our right
+            return ('leftof', rect2.left)
+        elif leftoverlap > 0 and leftoverlap <= abs(dx):
+            # dumped into rect2 on our left
+            return ('rightof', rect2.right)
+        else:
+            print("Collision detection fail!")
+    else:
+        return None
+
 p1 = Actor('p1_front')
 p1.pos = 100, 56
 p1.vy = 0
 p1.vx = 0
 p1.on_land = False
-p1.jump = False
 p1.walkindex = 0
 p1.keyleft = False
 p1.keyright = False
@@ -31,7 +72,7 @@ p1.keyright = False
 p1walk = ['p1_walk%02d' % (n + 1) for n in range(11)]
 p1walkl = ['p1_walkl%02d' % (n + 1) for n in range(11)]
 grounds = []
-grounds += make_ground(0, HEIGHT - images.ground.get_height(), WIDTH)
+grounds += make_ground(0, HEIGHT - images.ground.get_height(), WIDTH+50)
 grounds += make_ground(400, 400, 400)
 grounds += make_ground(25, 250, 250)
 grounds += make_ground(450, 150, 200)
@@ -39,7 +80,6 @@ grounds += make_ground(450, 150, 200)
 def on_key_down(key):
     if key == keys.UP and p1.on_land:
         p1.vy = JUMPSPEED
-        p1.jump = True
     if key == keys.LEFT:
         p1.keyleft = True
         p1.vx = -1 * P1WALK
@@ -60,47 +100,49 @@ def on_key_up(key):
             p1.vx = 0
 
 def update_player():
-    on_land = False
+    p1.on_land = False
+    # calculate how far we will move left or right
+    dx = p1.vx
+    # Calculate how far we will move up or down, 
+    # if we're moving at speed vy under influence of gravity
+    uy = p1.vy
+    p1.vy += GRAVITY
+    dy = (uy + p1.vy) / 2
+
     for g in grounds:
-        # Check if the player has bumped into any of the ground objects
-        if p1.colliderect(g):
-            # We've collided with a ground block - now we need
-            # to work out if we're above, below or to the side of it
-            if p1.vx != 0 and ((p1.midbottom[1] > g.midtop[1]+BUMPPOS and p1.midbottom[1] < g.midbottom[1]-BUMPPOS) or
-                               (p1.midtop[1] < g.midbottom[1]-BUMPPOS and p1.midtop[1] > g.midtop[1]+BUMPPOS)):
+        result = collide(p1, dx, dy, g)
+        if result:
+            if result[0]=='standing':
+                p1.on_land = True
+                # Correct our y position to be on top of the block
+                p1.bottom = g.top-1
+                if p1.vy > 0:
+                    # if we're standing on something, just slow our speed by the
+                    # amount we increased it or we will alternate between standing and not
+                    # and be unable to jump
+                    p1.vy -= GRAVITY
+                dy = 0
+            elif result[0]=='under':
+                print("hit my head!")
+                # if we've jumped upward and hit a block then stop going up and start falling
+                p1.top = g.bottom+1
+                p1.vy = BUMPSPEED
+                dy = 0
+            elif result[0]=='leftof':    
                 print("sideways block hit")
                 # if we're walking sideways and hit a block then stop
                 p1.vx = 0
-            elif p1.y <= g.y:
-                on_land = True
-                # Correct our y position to be on top of the block
-                p1.midbottom = (p1.x, g.midtop[1]+STANDPOS)
-            elif p1.y > g.y and not p1.on_land:
-                print("hit my head!")
-                # if we've jumped upward and hit a block then stop going up and start falling
-                p1.midtop = (p1.x, g.midbottom[1])
-                p1.vy = BUMPSPEED
+                p1.right = g.left-1
+                dx = 0
+            elif result[0]=='rightof':
+                print("sideways block hit")
+                # if we're walking sideways and hit a block then stop
+                p1.vx = 0
+                p1.left = g.right+1
+                dx = 0
 
-    if on_land and not p1.jump:
-        # We're walking or standing on a ground object
-        p1.on_land = True
-        p1.vy = 0
-        print("on land!", p1.vx)
-    else:
-        # We're either falling or jumping
-        if p1.jump:
-            print("jumping!")
-        else:
-            print("falling!")
-        p1.on_land = False
-        p1.jump = False
-        # Calculate our new y position, if we're moving at speed vy under influence of gravity
-        uy = p1.vy
-        p1.vy += GRAVITY
-        p1.y += (uy + p1.vy) / 2
-
-    # Move player left or right at speed vx
-    p1.x += p1.vx
+    p1.x += dx
+    p1.y += dy
 
     if p1.vx == 0:
         # If standing still show the front facing image
@@ -108,13 +150,23 @@ def update_player():
         p1.walkindex = 0
     elif p1.vx > 0:
         # If walking to the right, show each right facing image in sequence to animate
+        old_height = p1.height
+        old_width = p1.width
         p1.image = p1walk[p1.walkindex]
+        # not all the images are the same size, which can cause us to move through
+        # blocks if we don't correct for it
+        p1.x -= p1.width - old_width
+        p1.y -= p1.height - old_height
         p1.walkindex += 1
         if p1.walkindex > len(p1walk)-1:
             p1.walkindex = 0
     elif p1.vx < 0:
         # If walking to the left, show the left images in sequence
+        old_height = p1.height
+        old_width = p1.width
         p1.image = p1walkl[p1.walkindex]
+        p1.x += p1.width - old_width
+        p1.y -= p1.height - old_height
         p1.walkindex += 1
         if p1.walkindex > len(p1walk)-1:
             p1.walkindex = 0
